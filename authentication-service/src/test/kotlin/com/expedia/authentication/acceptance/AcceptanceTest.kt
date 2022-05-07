e19bab1088a6529947de0196396b3b6ca648447b
BAEKJungHo <designjava@naver.com>
2022-05-07 21:07:45 +0900

    [refactor] constructor di to lateinit var

--- authentication-service/src/test/kotlin/com/expedia/authentication/acceptance/AcceptanceTest.kt
+++ authentication-service/src/test/kotlin/com/expedia/authentication/acceptance/AcceptanceTest.kt
@@ -3,13 +3,14 @@ package com.expedia.authentication.acceptance
 import com.expedia.authentication.support.DatabaseCleanup
 import io.restassured.RestAssured
 import org.junit.jupiter.api.BeforeEach
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.web.server.LocalServerPort
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-class AcceptanceTest(
-    val databaseCleanup: DatabaseCleanup
-) {
+class AcceptanceTest {
+
+    @Autowired lateinit var databaseCleanup: DatabaseCleanup
 
     @LocalServerPort
     lateinit var port: String
--- authentication-service/src/test/kotlin/com/expedia/authentication/acceptance/user/UserAcceptanceTest.kt
+++ authentication-service/src/test/kotlin/com/expedia/authentication/acceptance/user/UserAcceptanceTest.kt
@@ -1,13 +1,9 @@
 package com.expedia.authentication.acceptance.user
 
 import com.expedia.authentication.acceptance.AcceptanceTest
-import com.expedia.authentication.support.DatabaseCleanup
 import org.junit.jupiter.api.Test
-import org.springframework.beans.factory.annotation.Autowired
 
-internal class UserAcceptanceTest(
-    @Autowired databaseCleanup: DatabaseCleanup
-): AcceptanceTest(databaseCleanup) {
+internal class UserAcceptanceTest: AcceptanceTest() {
 
     @Test
     fun `회원 가입을 한다`() {
