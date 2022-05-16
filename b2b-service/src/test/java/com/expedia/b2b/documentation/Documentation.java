60c6d944d8eff68505e0fa466d53bd5a7ce60ba9
westssum <seohae0406@gmail.com>
2022-05-16 20:26:56 +0900

    [feature] 숙박업체 생성 및 조회 - 문서화 테스트 작성 (1)

---
+++ b2b-service/src/test/java/com/expedia/b2b/documentation/Documentation.java
@@ -0,0 +1,27 @@
+package com.expedia.b2b.documentation;
+
+import io.restassured.RestAssured;
+import io.restassured.specification.RequestSpecification;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.context.annotation.Import;
+import org.springframework.restdocs.RestDocumentationExtension;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ExtendWith(RestDocumentationExtension.class) // 스니펫 생성을 위한 선언
+@Import(DocumentationConfiguration.class)
+@AutoConfigureRestDocs
+public class Documentation {
+    @LocalServerPort
+    int port;
+
+    protected RequestSpecification spec;
+
+    @BeforeEach
+    public void setUp() {
+        RestAssured.port = port;
+    }
+}
