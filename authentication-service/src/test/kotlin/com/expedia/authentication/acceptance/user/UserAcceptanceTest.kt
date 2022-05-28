45a7997e0f16e9e1c36769367ec1464fa29b40ef
BAEKJungHo <designjava@naver.com>
2022-05-28 18:03:10 +0900

    문서화 작성 중

--- authentication-service/src/test/kotlin/com/expedia/authentication/acceptance/user/UserAcceptanceTest.kt
+++ authentication-service/src/test/kotlin/com/expedia/authentication/acceptance/user/UserAcceptanceTest.kt
@@ -7,7 +7,7 @@ internal class UserAcceptanceTest: AcceptanceTest() {
 
     @Test
     fun `회원 가입을 한다`() {
-        val response = `회원 생성 요청`("designjava@naver.com", "jungho")
+        val response = `회원 생성 요청`()
         `회원이 생성 됨`(response)
     }
 }
