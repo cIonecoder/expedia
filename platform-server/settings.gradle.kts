5e3fcc5fa5156fde5098e8a2e6ff5cedab99a5ff
BAEKJungHo <designjava@naver.com>
2022-09-23 18:19:41 +0900

    feat: Platform Server - Multi Module 설정

--- platform-server/settings.gradle.kts
+++ platform-server/settings.gradle.kts
@@ -1,1 +1,18 @@
-rootProject.name = "authentication"
+pluginManagement {
+    val kotlinVersion: String by settings
+    val springBootVersion: String by settings
+    val springDependencyManagementVersion: String by settings
+    val asciiDoctorVersion: String by settings
+
+    plugins {
+        id("org.springframework.boot") version springBootVersion
+        id("io.spring.dependency-management") version springDependencyManagementVersion
+        id("org.asciidoctor.jvm.convert") version asciiDoctorVersion
+        kotlin("jvm") version kotlinVersion
+        kotlin("plugin.spring") version kotlinVersion
+        kotlin("plugin.jpa") version kotlinVersion
+    }
+}
+
+rootProject.name = "platform-server"
+include("auth-server")
