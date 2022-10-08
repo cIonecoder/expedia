9de2af5ccae0e2c3a92ce224dbb41508269a27ae
kimseohae <202083@ssg.com>
2022-10-08 14:54:02 +0900

    [feature#30] Expedia Server - 프로젝트 환경설정 (package common setting)

--- expedia-server/booking-server/src/main/java/org/clonecoder/bookingserver/common/enums/EnumMessage.java
+++ expedia-server/booking-server/src/main/java/org/clonecoder/bookingserver/common/enums/EnumMessage.java
@@ -3,6 +3,7 @@ package org.clonecoder.bookingserver.common.enums;
 import lombok.Getter;
 
 public enum EnumMessage {
+    HTTP_SUCCESS      ("성공"),
     NOT_EXIST_BOOKING ("존재하지않은 예약건입니다.")
     ;
 
