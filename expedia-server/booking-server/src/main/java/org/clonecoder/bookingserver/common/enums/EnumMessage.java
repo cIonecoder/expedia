7a0c558041256c5649ab67eddac59f25dc4bc000
kimseohae <202083@ssg.com>
2022-10-08 17:52:54 +0900

    feature37: Booking Server ResponseDto 코드 정리

--- expedia-server/booking-server/src/main/java/org/clonecoder/bookingserver/common/enums/EnumMessage.java
+++ expedia-server/booking-server/src/main/java/org/clonecoder/bookingserver/common/enums/EnumMessage.java
@@ -3,7 +3,6 @@ package org.clonecoder.bookingserver.common.enums;
 import lombok.Getter;
 
 public enum EnumMessage {
-    HTTP_SUCCESS      ("성공"),
     NOT_EXIST_BOOKING ("존재하지않은 예약건입니다.")
     ;
 
