a3f8559032d1e8f32c12b23f94f0257362564bd4
jungho.baek <jungho.baek@42dot.ai>
2022-10-09 15:57:12 +0900

    feat: JpaAuditingConfig

---
+++ platform-server/member-server/src/main/kotlin/org/clonecoder/member/config/JpaAuditingConfig.kt
@@ -0,0 +1,9 @@
+package org.clonecoder.member.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing
+
+@EnableJpaAuditing
+@Configuration
+class JpaAuditingConfig {
+}
