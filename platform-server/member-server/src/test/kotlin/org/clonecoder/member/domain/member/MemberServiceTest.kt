07951b157477145e2c24516b3e42be694d2e7354
jungho.baek <jungho.baek@42dot.ai>
2022-10-09 14:35:24 +0900

    feat: 로그인 도메인 테스트 코드 작성 완료

--- platform-server/member-server/src/test/kotlin/org/clonecoder/member/domain/member/MemberServiceTest.kt
+++ platform-server/member-server/src/test/kotlin/org/clonecoder/member/domain/member/MemberServiceTest.kt
@@ -9,9 +9,16 @@ import io.mockk.impl.annotations.InjectMockKs
 import io.mockk.impl.annotations.MockK
 import io.mockk.junit5.MockKExtension
 import io.mockk.just
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import org.assertj.core.api.Assertions.assertThat
 import org.clonecoder.member.common.exception.DuplicatedEmailException
 import org.clonecoder.member.domain.member.adapter.AuthAdapter
+import org.clonecoder.member.payload.accessToken
+import org.clonecoder.member.payload.email
 import org.clonecoder.member.payload.issueTokenPayloads
+import org.clonecoder.member.payload.refreshToken
+import org.clonecoder.member.payload.validPassword
 import org.junit.jupiter.api.assertThrows
 import org.junit.jupiter.api.extension.ExtendWith
 
@@ -29,10 +36,23 @@ internal class MemberServiceTest: DescribeSpec() {
 
     init {
         describe("회원 가입") {
-            val validMember = MemberCommand.RegisterMember(email = DUPLICATED_EMAIL, password = "123!abACC123")
+            val duplicatedMember = MemberCommand.RegisterMember(email = DUPLICATED_EMAIL, password = validPassword)
 
             it("이메일이 중복되는 경우 회원 가입 실패") {
-                assertThrows<DuplicatedEmailException> { memberService.register(validMember) }
+                assertThrows<DuplicatedEmailException> { memberService.register(duplicatedMember) }
+            }
+        }
+
+        describe("로그인") {
+            val validMember = MemberCommand.LoginRequest(email = email, password = validPassword)
+
+            it("회원이 존재하면 로그인 성공") {
+                val loginMember = withContext(Dispatchers.IO) {
+                    memberService.login(validMember)
+                }
+
+                assertThat(loginMember.accessToken).isEqualTo(accessToken)
+                assertThat(loginMember.refreshToken).isEqualTo(refreshToken)
             }
         }
     }
@@ -43,6 +63,8 @@ internal class MemberServiceTest: DescribeSpec() {
 
         every { memberReader.existsByEmail(DUPLICATED_EMAIL) } returns true
         every { memberStore.register(any()) } just Runs
+
         every { authAdapter.issueTokens(any()) } returns issueTokenPayloads()
+        every { memberReader.existsByEmailAndPassword(any(), any()) } returns true
     }
 }
--- platform-server/member-server/src/test/kotlin/org/clonecoder/member/payload/MemberPayload.kt
+++ platform-server/member-server/src/test/kotlin/org/clonecoder/member/payload/MemberPayload.kt
@@ -6,6 +6,8 @@ import org.clonecoder.member.interfaces.member.dto.MemberDto
 const val email = "designjava@naver.com"
 const val validPassword = "123!abACC123"
 const val invalidPassword = "1234"
+const val accessToken = "test_accessToken"
+const val refreshToken = "test_refreshToken"
 
 fun memberRegisterSuccessPayloads(): MemberDto.RegisterRequest {
     return MemberDto.RegisterRequest(
@@ -30,8 +32,8 @@ fun memberLoginSuccessPayloads(): MemberDto.LoginRequest {
 
 fun issueTokenPayloads(): AuthApiCallerDto.IssueTokensResponse {
     return AuthApiCallerDto.IssueTokensResponse(
-        accessToken = "test_accessToken",
-        refreshToken = "test_refreshToken",
+        accessToken = accessToken,
+        refreshToken = refreshToken,
         email = email,
         status = AuthApiCallerDto.TokenResponseStatus.SUCCESS
     )
