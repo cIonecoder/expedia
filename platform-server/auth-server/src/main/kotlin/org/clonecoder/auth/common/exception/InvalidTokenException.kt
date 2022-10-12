7a7ae30035c9285c107887f15a51f87c739a1976
BAEKJungHo <designjava@naver.com>
2022-10-13 01:00:22 +0900

    feature: Exception Hierarchy 리팩토링

--- platform-server/auth-server/src/main/kotlin/org/clonecoder/auth/common/exception/InvalidTokenException.kt
+++ platform-server/auth-server/src/main/kotlin/org/clonecoder/auth/common/exception/InvalidTokenException.kt
@@ -3,6 +3,5 @@ package org.clonecoder.auth.common.exception
 import org.clonecoder.auth.common.response.ErrorCode
 
 class InvalidTokenException(
-    val errorCode: ErrorCode = ErrorCode.INVALID_TOKEN,
-    tokenName: String
-): RuntimeException("Invalid $tokenName")
+    override val errorCode: ErrorCode = ErrorCode.INVALID_TOKEN,
+): DomainException(errorCode)
--- platform-server/auth-server/src/main/kotlin/org/clonecoder/auth/common/response/ErrorCode.kt
+++ platform-server/auth-server/src/main/kotlin/org/clonecoder/auth/common/response/ErrorCode.kt
@@ -1,5 +1,6 @@
 package org.clonecoder.auth.common.response
 
+import org.slf4j.event.Level
 import org.springframework.http.HttpStatus
 
 
@@ -7,14 +8,20 @@ enum class ErrorCode(
     val status: HttpStatus,
     val code: String,
     val message: String,
+    val logLevel: Level = Level.INFO
 ) {
     // Well known Errors
-    BAD_REQUEST(HttpStatus.BAD_REQUEST, "-1004", "Bad Request"),
-    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "-1005", "Internal Server Error"),
-    VALIDATION(HttpStatus.BAD_REQUEST, "-1006", "Validation Error"),
+    BAD_REQUEST(HttpStatus.BAD_REQUEST, "W1004", "Bad Request", Level.ERROR),
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "W1005", "Internal Server Error", Level.ERROR),
 
     // Custom Errors
-    INVALID_TOKEN(HttpStatus.BAD_REQUEST, "-2001", "Invalid Token"),
+    VALIDATION(HttpStatus.BAD_REQUEST, "C1001", "Validation Error"),
+    INVALID_TOKEN(HttpStatus.BAD_REQUEST, "C1002", "Invalid Token"),
+    INVALID_ACCESS_TOKEN(HttpStatus.BAD_REQUEST, "C1003", "Invalid Access Token"),
+    INVALID_REFRESH_TOKEN(HttpStatus.BAD_REQUEST, "C1004", "Invalid Refresh Token"),
+    REDIS_CLIENT(HttpStatus.BAD_REQUEST, "C1005", "Redis Client Error", Level.ERROR),
 
+    // Hierarchy
+    DOMAIN(HttpStatus.BAD_REQUEST, "H1001", "Domain Layer Error"),
     ;
 }
